3a206045d8a030df59fa618dd5bbb52fd2d41ab2
jennifer jiang <nfjennifer.2009@gmail.com>
2021-08-30 11:12:20 -0400

    remove timeout=10(seconds) to enable default timeout (few minutes) to connect server

--- utils/test_utils.py
+++ utils/test_utils.py
@@ -5,7 +5,7 @@ class TestUtils:
 
     @staticmethod
     def verify_links(link_url):
-        response = requests.get(link_url, timeout=10)
+        response = requests.get(link_url)
         status_code = response.status_code
         if status_code != 200:
             print("........broken link: " + link_url + "status_code: " + status_code)
