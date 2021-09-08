a00a0c4abbada471a2d153e78f01283f4b02ccbc
jennifer jiang <nfjennifer.2009@gmail.com>
2021-09-08 15:48:22 -0400

    added conftest.py on mls

---
+++ tests/mls/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+from pages.mls.search_mls_container import SearchMlsContainer
+
+
+@pytest.fixture()
+def mls_setup(setup, config):
+    search_mls_container = SearchMlsContainer(setup)
+    search_mls_container.open_home_page(config)
+    search_mls_container.wait_mapbox_loaded()
+
