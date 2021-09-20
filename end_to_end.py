a996fe5ca3a0f74d8e05283dd59954a5db13630f
jennifer jiang <nfjennifer.2009@gmail.com>
2021-09-20 19:05:02 -0400

    test

--- end_to_end.py
+++ end_to_end.py
@@ -1,10 +1,49 @@
-from selenium import webdriver
-import pytest
+import datetime
+import json
+
+import re
+from datetime import date
+
+from utils.test_utils import TestUtils
 
 
-class TestOne:
+class TestOne():
 
-    def test_url(self):
-        self.driver = webdriver.PhantomJS()
-        self.driver.set_window_size(1120, 550)
-        self.driver.get("http://duckduckgo.com/")
+    def test_convertion(self):
+        t = ["10", "12", "22"]
+        for i in t:
+            print(i)
+            if i == "10":
+                continue
+
+
+
+
+
+
+
+
+    def atest_url(self):
+        url = "https://house.51.ca/mls?buildingType=2%2C3&region=44.20128885967054%2C-79.89050315188348%2C43.41440746065908%2C-78.97863791750851&transactionType=1&limit=200"
+        url = "https://house.51.ca/mls?region=44.17568615978678%2C-79.86921714114095%2C43.44033849670947%2C-78.99992392825044&transactionType=1&limit=200"
+        url = "https://house.51.ca/mls?region=44.17568615978678%2C-79.86921714114095%2C43.44033849670947%2C-78.99992392825044&transactionType=1&limit=200"
+        region = {}
+        content = url.split("?")[1]
+        contents = content.split("&")
+        for c in contents:
+            ary = c.split("=")
+            key = ary[0]
+            value = ary[1].replace("%2C%20", " ")
+            value = value.replace("%20", " ")
+            if key == "buildingType":
+                value = value.replace("%2C", ",")
+            if key != "region":
+                region[key] = value
+
+        print(str(region))
+        s = str(region)
+        print(type(region))
+        print(type(s))
+        print(s == str(region))
+        d = json.dumps(region)
+        print(type(d))
---
+++ tests/houseHome/test_selected_city_by_user.py
@@ -0,0 +1,22 @@
+from utils.read_json_house_home import JsonReader
+from pages.HouseHomePage.home_select_city import HouseHomePage
+import allure
+import pytest
+
+
+@pytest.mark.usefixtures("home_page_setup")
+class TestSelectedCity:
+    catalogdata = JsonReader.get_catalog_data()
+
+    @allure.title("房屋首页 - 测试每个省，确认下拉菜单显示数据完整")
+    @allure.description(" 打开选择城市的下拉菜单，验证： 下拉菜单显示数据是正确的")
+    @pytest.mark.parametrize("catalogObject", catalogdata)
+    def test_selected_cities(self, catalogObject):
+        province = catalogObject["province"]
+        city = catalogObject["city"]
+
+        # 1 select a city
+        home_page = HouseHomePage(self.driver)
+        home_page.click_select_city_button()
+        home_page.select_province(province)
+        home_page.select_city(city)
