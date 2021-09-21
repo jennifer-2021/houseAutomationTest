71590366c02b644958f7d16af280056a833b0009
jennifer jiang <nfjennifer.2009@gmail.com>
2021-09-21 11:09:04 -0400

    debug - UI element changed

--- locators/HouseHomePage/locators_house_home_page.py
+++ locators/HouseHomePage/locators_house_home_page.py
@@ -6,7 +6,7 @@ class SetHouseHomePageLocators:
     select_city_open_button = (By.CSS_SELECTOR, "#header-city-selector-root>div>button")
     search_box = (By.CSS_SELECTOR, "[class*='__searchInput'] input")
     province_list = (By.CSS_SELECTOR, "[class*='provinceWrap'] div:nth-child(1)")
-    city_list = (By.CSS_SELECTOR, "[class*='city--1zUho'] [data-id]")
+    city_list = (By.CSS_SELECTOR, ".CitySelector-module__cityWrap--gcLfY [class*='city--3EVAd']>div")
 
     # modal
     select_area_modal = (By.CSS_SELECTOR, "//h5[text()='搜索或选择您关注的城市']")
